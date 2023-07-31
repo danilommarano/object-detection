5af6df333845d2ed805657890b3d7ac9155c07ca
Danilo Marano <danilo.m.marano@gmail.com>
2023-07-30 22:39:24 -0300

    Set TechButton to be more bright

--- src/components/TechButton.svelte
+++ src/components/TechButton.svelte
@@ -3,7 +3,7 @@
   export let logoSrc: string;
 </script>
 
-<button class="flex h-6 items-center gap-1/2 px-2 w-fit rounded-full border bg-white shadow-sm cursor-poniter hover:bg-gradient-to-b hover:from-white hover:to-gray-200">
+<button class="flex h-6 items-center gap-1/2 px-2 w-fit rounded-full border bg-white shadow-sm cursor-poniter hover:bg-gradient-to-b hover:from-white hover:to-gray-100">
   <img class="h-4 w-4 mr-2" src={logoSrc} alt="Technology Logo" />
   <span class="font-semibold text-sm ">{techName}</span>
 </button>
