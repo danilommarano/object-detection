47064586680620a176a2ffe36bb13ded834024c5
Danilo Marano <danilo.m.marano@gmail.com>
2023-07-30 22:40:36 -0300

    Disbled Navbar having children

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,31 @@
-<nav class='fixed flex justify-center border-b h-16 w-full'>
+<script>
+    import Logo from '../components/Logo.svelte';
+    import Tabs from '../components/Tabs.svelte';
+    import Tab from '../components/Tab.svelte';
+
+  const links = [
+    { label: 'Análises', emoji: '📉' },
+    { label: 'Sistemas', emoji: '🖥️' },
+    { label: 'Modelos', emoji: '🧠' },
+    { label: 'Blog', emoji: '📝' },
+  ]
+
+</script>
+
+<nav class='fixed flex justify-center border-b h-16 w-full bg-white'>
     <div class='container flex justify-between items-center h-full'>
-        <slot />
+    <Logo/>
+    <Tabs class='flex items-end gap-4 h-full'>
+      {#each links as link}
+        <Tab
+            class="flex items-center p-3 border-b gap-2 font-semibold text-gray-500 border-transparent hover:border-gray-800 hover:text-gray-800 active:border-gray-800 selection:border-gray-800"
+            on:click={() => alert(link.label)}
+        >
+          <p>{link.emoji}</p>
+          <p>{link.label}</p>
+        </Tab>
+      {/each}
+    </Tabs>
     </div>
 </nav>
     
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,63 +15,50 @@
 
   let activePanel:string = tabs[0]['label'];
 
-    function handleTabClick(tab_label:string) {
-      activePanel = tab_label;
-    }
-  </script>
+  function handleTabClick(tab_label:string) {
+    activePanel = tab_label;
+  }
+</script>
 
-  <main class='flex justify-center text-gray-800'>
-    <Navbar>
-      <Logo/>
-      <div>
-          <div class='flex gap-8 text-lg font-semibold'>
-              <a class=''>Análises</a>
-              <a class=''>Sistemas</a>
-              <a class=''>Websites</a>
-              <a class=''>Modelos</a>
-              <a class=''>Blog</a>
-          </div>
-      </div>
-    </Navbar>
-    <div class='w-full h-screen mt-16'>
-        <div class='flex justify-center items-end h-48 bg-gradient-to-b from-white to-gray-50'>
+<main class='flex justify-center text-gray-800'>
+  <Navbar />
+  <div class='w-full h-screen mt-16'>
+      <div class='flex justify-center items-end h-48 bg-gradient-to-b from-white to-gray-50'>
           <div class='flex flex-col gap-6 container text-gray-500'>
               <div class='flex flex-col gap-3'>
                 <div class='flex flex-col'>
                   <BreadCrumb items={["Computação", "Inteligência Artificial"]} />
                   <h1 class='font-bold text-4xl text-gray-600'>Detecção de objetos usando o COCO-SSD</h1>
-                  </div>
-                  <BadgesList>
-                    <TechButton techName={'TypeScript'} logoSrc={'vscode-icons-file-type-typescript-official.svg'} />
-                    <TechButton techName={'Svelte'} logoSrc={'vscode-icons-file-type-svelte.svg'} />
-                    <TechButton techName={'Tailwind'} logoSrc={'devicon-tailwindcss.svg'} />
-                    <TechButton techName={'Tensorflow'} logoSrc={'devicon-tensorflow.svg'} />
-                    <TechButton techName={'Python'} logoSrc={'vscode-icons-file-type-python.svg'} />
-                  </BadgesList>
                 </div>
-                  <Tabs>
-                    {#each tabs as tab}
-                      <Tab on:click={() => handleTabClick(tab.label)}>
-                        <p>{tab.emoji}</p>
-                        <p>{tab.label}</p>
-                      </Tab>
-                    {/each}
-                  </Tabs>
-            </div>
-        </div>
+                <BadgesList>
+                  <TechButton techName={'Tensorflow'} logoSrc={'devicon-tensorflow.svg'} />
+                  <TechButton techName={'TypeScript'} logoSrc={'vscode-icons-file-type-typescript-official.svg'} />
+                  <TechButton techName={'Svelte'} logoSrc={'vscode-icons-file-type-svelte.svg'} />
+                  <TechButton techName={'Tailwind'} logoSrc={'devicon-tailwindcss.svg'} />
+                </BadgesList>
+              </div>
+                <Tabs>
+                  {#each tabs as tab}
+                    <Tab on:click={() => handleTabClick(tab.label)}>
+                      <p>{tab.emoji}</p>
+                      <p>{tab.label}</p>
+                    </Tab>
+                  {/each}
+                </Tabs>
+          </div>
+      </div>
 
-        <div class='flex justify-center'>
+      <div class='flex justify-center'>
           <div class='container w-full pt-4'>
-              {#if activePanel === tabs[0]['label']}
+            {#if activePanel === tabs[0]['label']}
               <ObjectDetectionCamera />
-              {/if}
+            {/if}
 
 
-              {#if activePanel === tabs[1]['label']}
-              teste
-              {/if}
-            </div>
-        </div>
-    </div>
-  </main>
-  
+            {#if activePanel === tabs[1]['label']}
+            teste
+            {/if}
+          </div>
+      </div>
+  </div>
+</main>
